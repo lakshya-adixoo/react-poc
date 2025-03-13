e4b1bf0e3e34884305ee10730e395b2c69dac976
lakshya-adixoo <lakshya.shrivastava@adixoo.com>
2025-03-13 15:55:29 +0530

    added some new concept POC

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,105 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios';
 
-function App() {
+// function App() {
 
 
-  //Code of counter 
+//   //Code of counter 
 
-  // const [count , setCount] = useState(0);
+//   // const [count , setCount] = useState(0);
 
-  //  function increase(){
-  //     setCount(count+1);
-  //  }
+//   //  function increase(){
+//   //     setCount(count+1);
+//   //  }
 
-  //  function decrease(){
-  //   if(count >0){
-  //     setCount(count-1);
-  //   }
-  //   else{
-  //     alert("Counter cannot less than 0")
-  //   }
-  //    }
+//   //  function decrease(){
+//   //   if(count >0){
+//   //     setCount(count-1);
+//   //   }
+//   //   else{
+//   //     alert("Counter cannot less than 0")
+//   //   }
+//   //    }
   
-  // return (
-  //   <>
-  //   <div>
-  //     counter : {count}
-  //     <button onClick={increase}>Increase count</button>
-  //     <button onClick={decrease}>Decrease count</button>
-  //   </div>
-  //   </>
-  // )
+//   // return (
+//   //   <>
+//   //   <div>
+//   //     counter : {count}
+//   //     <button onClick={increase}>Increase count</button>
+//   //     <button onClick={decrease}>Decrease count</button>
+//   //   </div>
+//   //   </>
+//   // )
 
 
-  //code for data fetching from an api
+//   //code for data fetching from an api
 
-  // const [data , setData] = useState([]);
+//   // const [data , setData] = useState([]);
 
-  // useEffect(()=>{
+//   // useEffect(()=>{
 
-  //    axios.get('https://jsonplaceholder.typicode.com/posts')
-  //   .then(response => setData(response.data))
+//   //    axios.get('https://jsonplaceholder.typicode.com/posts')
+//   //   .then(response => setData(response.data))
      
-  // },[])
+//   // },[])
 
 
-  // return (
-  //   <>
-  //   <h1>Data from API</h1>
-  //   <ul>
-  //       {data.map((item) => (
-  //         <li key={item.id}>{item.title}</li> 
-  //       ))}
-  //     </ul>
-  //   </>
-  // )
+//   // return (
+//   //   <>
+//   //   <h1>Data from API</h1>
+//   //   <ul>
+//   //       {data.map((item) => (
+//   //         <li key={item.id}>{item.title}</li> 
+//   //       ))}
+//   //     </ul>
+//   //   </>
+//   // )
+
+// //   function getData(val){
+// //       console.log(val.target.value)
+// //   }
   
+// //  return (
+// //   <>
+// //   <input onChange={getData}></input>
+// //   </>
+// //  )
 
 
-}
+// function clickButton(){
+//   alert("hello world")
+// }
 
-export default App
+// return (
+//   <>
+//   <button onButtonClick={clickButton}>Click here</button>
+
+//   <button onClick={onbu}>Click me as well</button>
+//   </>
+// )
+
+// }
+
+// export default App
+
+
+
+// code for the Passing event handlers as props
+// import React from "react";
+// import Child from "./child";
+
+// function App() {
+//   function clickButton() {
+//     alert("Hello, world!");
+//   }
+
+//   return (
+//     <>
+//       <Child onButtonClick={clickButton} />
+//     </>
+//   );
+// }
+
+// export default App;
+
+
---
+++ src/child.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Child({ onButtonClick }) {
+  return <button onClick={onButtonClick}>Click Me</button>;
+}
+
+export default Child;
